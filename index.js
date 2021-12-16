af18aedf4f97450b1662ba9868dd0db8ab0bc586
Mahmut Furkan ÇÖKEK <44062559+mfcokek@users.noreply.github.com>
2021-12-16 09:59:22 +0300

    Ödev Yüklemesi
    Şuan bonus yok

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const isCreditCardNumberValid = (cardNumber) => {
+  var sum = 0;
+  var only = 0;
+    // Girilen veride - işareti temizlenir.
+          if(cardNumber[4]=='-' && cardNumber[9]=='-' && cardNumber[14]=='-'){
+              cardNumber = cardNumber.slice(0,4) + cardNumber.slice(5,9) + cardNumber.slice(10,14) + cardNumber.slice(15,19);
+            }
 
+    // Uzunluğu ve harf kontolü yapılır.
+    if(cardNumber.length == 16 && cardNumber.toUpperCase() == cardNumber && cardNumber.toLowerCase()  == cardNumber){
+      //veri sayı formatına çevrilir ve her bir basamak toplanır.
+      cardNumber = +cardNumber;
+      for(var j = 0;j<16;j++){
+        sum = sum + (+cardNumber.toString()[j]);
+      }
+    }
+    // toplam, çift mi ve her bir basamak aynı mı sorgusu yapılır.
+    only = +cardNumber.toString()[0]
+    if(sum>=16 && cardNumber%2==0 && sum/only != 16){
+      return true;
+    }
+    return false;
 }
+
+console.log(isCreditCardNumberValid('6666-6666-6666-1666'));
